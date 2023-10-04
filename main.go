89ec62b72fcab0926cf3f0bf623d0fbc0eac172b
PaulFHTW <paul.bogza@gmail.com>
2023-10-04 13:43:28 +0200

    attempt

--- main.go
+++ main.go
@@ -4,15 +4,21 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
+	var files [1024]string
+
 	entries, err := os.ReadDir("./")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _, e := range entries {
-		fmt.Println(e.Name())
+	for index, e := range entries {
+		files[index] = e.Name()
+		fmt.Println(strings.Split(files[index], "."))
 	}
 }
+
+/*"gswin64c -dPDFA -dBATCH -dNOPAUSE -sColorConversionStrategy=UseDeviceIndependentColor -sDEVICE=pdfwrite -dPDFACompatibilityPolicy=2 -sOutputFile=" + '"' + sPDF + '" "' + sPDFA + '"'*/
